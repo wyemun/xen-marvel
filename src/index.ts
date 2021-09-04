9e29eabe01d2f16c26a756296c014b1ac2167b25
wyemun <wyemun@gmail.com>
2021-09-04 12:51:26 +0000

    Fixed setup.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import cluster from 'cluster'
 import os from 'os'
 import App from './App'
 import Locals from './providers/Locals'
+import dotenv from 'dotenv'
+
+/**
+ * Init local .env
+ */
+dotenv.config() // TODO conditional for dev only
 
 /**
  * Make use of multi core CPU to scale up app instances
