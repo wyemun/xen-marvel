3d340a1dd1215065b4b32715c160ee7e955f4891
wyemun <wyemun@gmail.com>
2021-09-04 14:12:20 +0000

    API response now as designed/

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -5,34 +5,58 @@ interface GetCharacterByIdParam {
   characterId: string
 }
 
+interface MarvelRespCharacter {
+  id: number,
+  name: string,
+  description: string,
+  modified: string
+}
+
+interface MarvelResp {
+  code: number,
+  etag: string,
+  data: {
+    offset: number
+    limit: number
+    total: number
+    count: number
+    results: MarvelRespCharacter[]
+  }
+}
+
 export default class ApiController {
   public static async getCharacterById(req: Request<GetCharacterByIdParam>, res: Response): Promise<any> {
     const characterId = req.params.characterId
 
     const mRes = await Marvel.getCharacterById(characterId)
-    const mJson = await mRes.json()
+    const mJson: MarvelResp = await mRes.json()
     
-    if (mRes.status < 200 || mRes.status >= 300) {
+    if (mRes.status < 200 || mRes.status >= 300 || mJson.data?.total < 1) {
       console.log('Marvel API failed: %s', mRes.status)
       console.log('Response: ', mJson)
       throw new Error('Failed to get request')
     }
 
-    return res.json(mJson) // TODO clean it up?
+    const character = mJson.data.results[0]
+
+    return res.json({
+      Id: character.id,
+      Name: character.name,
+      Description: character.description
+    })
   }
 
   public static async getAllCharacters(req: Request, res:Response): Promise<any> {
-    const characterId = req.params.characterId
-
     const mRes = await Marvel.getAllCharacters()
-    const mJson = await mRes.json()
+    const mJson: MarvelResp = await mRes.json()
     
-    if (mRes.status < 200 || mRes.status >= 300) {
+    if (mRes.status < 200 || mRes.status >= 300) { // TODO handle ETAG cache 304
       console.log('Marvel API failed: %s', mRes.status)
       console.log('Response: ', mJson)
       throw new Error('Failed to get request')
     }
 
-    return res.json(mJson) // TODO clean it up?
+    const characterIds = mJson.data.results.map(({id}) => id)
+    return res.json(characterIds)
   }
 }
